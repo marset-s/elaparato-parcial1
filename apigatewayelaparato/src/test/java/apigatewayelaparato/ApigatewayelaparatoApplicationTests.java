1ddf1f47ec42a3e51ce83ba4c5e06e47763b2ec4
Serrana Marset <serranamarset@gmail.com>
2024-05-29 14:40:29 -0300

    Se carga api-gatway - Se configura app-elaparato para autenticación con keycloak

---
+++ apigatewayelaparato/src/test/java/apigatewayelaparato/ApigatewayelaparatoApplicationTests.java
@@ -0,0 +1,13 @@
+package apigatewayelaparato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayelaparatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
